8380317370b290acd90d26eb1990a2b0eb862e42
student <student@PC-xxx.urel.feec.vutbr.cz>
2020-02-12 14:32:52 +0100

    first part - led blinking without debouncing

--- cv2/Src/main.c
+++ cv2/Src/main.c
@@ -17,7 +17,7 @@ void EXTI0_1_IRQHandler(void)
 {
 	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
 		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
-		GPIOB->ODR ^= (1<<5);
+		GPIOB->ODR ^= (1<<0);
 	}
 }
 
@@ -27,7 +27,7 @@ int main(void)
 	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
 	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
-	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_1; // S1 = PC1, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // enable clock for syscfg
 	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0

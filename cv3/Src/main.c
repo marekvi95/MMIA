49ac05cd8578a8e79fe5dbfdb68805738a5309cd
student <student@PC-xxx.urel.feec.vutbr.cz>
2020-02-19 15:26:43 +0100

    displaying number on display

--- cv3/Src/main.c
+++ cv3/Src/main.c
@@ -17,6 +17,11 @@ int main(void)
 	sct_init();
 	sct_led(0x7A5C36DE);
 	for (volatile uint32_t i = 0; i < 1000000; i++);
+	for(int i=111; i<=999; i+=111) {
+		sct_value(i);
+		for (volatile uint32_t i = 0; i < 1000000; i++);
+	}
+
 
 	while(1) {
 
--- cv3/Src/sct.c
+++ cv3/Src/sct.c
@@ -13,22 +13,69 @@
 #define sct_clk(x) do { if (x) GPIOB->BSRR = (1 << 3); else GPIOB->BRR = (1 << 3); } while (0)
 #define sct_noe(x) do { if (x) GPIOB->BSRR = (1 << 10); else GPIOB->BRR = (1 << 10); } while (0)
 
+void sct_value(uint16_t value) {
+	uint32_t reg = 0;
+
+	static const uint32_t reg_values[3][10] = {
+			{
+					//PCDE--------GFAB @ DIS1
+					0b0111000000000111 << 16,
+					0b0100000000000001 << 16,
+					0b0011000000001011 << 16,
+					0b0110000000001011 << 16,
+					0b0100000000001101 << 16,
+					0b0110000000001110 << 16,
+					0b0111000000001110 << 16,
+					0b0100000000000011 << 16,
+					0b0111000000001111 << 16,
+					0b0110000000001111 << 16,
+			},
+			{ 		//----PCDEGFAB---- @ DIS2
+					0b0000011101110000 << 0,
+					0b0000010000010000 << 0,
+					0b0000001110110000 << 0,
+					0b0000011010110000 << 0,
+					0b0000010011010000 << 0,
+					0b0000011011100000 << 0,
+					0b0000011111100000 << 0,
+					0b0000010000110000 << 0,
+					0b0000011111110000 << 0,
+					0b0000011011110000 << 0,
+			},
+			{
+					//PCDE--------GFAB @ DIS3
+					0b0111000000000111 << 0,
+					0b0100000000000001 << 0,
+					0b0011000000001011 << 0,
+					0b0110000000001011 << 0,
+					0b0100000000001101 << 0,
+					0b0110000000001110 << 0,
+					0b0111000000001110 << 0,
+					0b0100000000000011 << 0,
+					0b0111000000001111 << 0,
+					0b0110000000001111 << 0,
+			},
+	};
+
+	reg |= reg_values[0][value / 100 % 10];
+	reg |= reg_values[1][value / 10 % 10];
+	reg |= reg_values[2][value % 10];
+
+	sct_led(reg);
+
+
+}
 
 void sct_led(uint32_t value)
 {
-	while(value) {
+	for(int i=0; i<32; i++) {
 
-		if (value & 1)
-			sct_sdi(1); // set
-		else
-			sct_sdi(0);
-//		for (volatile uint32_t i = 0; i < 10000; i++);
+		sct_sdi(value & 1); // set
 		sct_clk(1);
-//		for (volatile uint32_t i = 0; i < 10000; i++);
 		sct_clk(0);
-//		for (volatile uint32_t i = 0; i < 10000; i++);
 		value >>= 1;
 	}
+
 	sct_nla(1);
 	sct_nla(0);
 }
@@ -40,8 +87,9 @@ void sct_init(void)
 	GPIOB -> MODER |= GPIO_MODER_MODER4_0;
 	GPIOB -> MODER |= GPIO_MODER_MODER5_0;
 	GPIOB -> MODER |= GPIO_MODER_MODER10_0;
-	sct_led(0);
 	sct_noe(0);
+	sct_led(0);
+
 }
 
 
